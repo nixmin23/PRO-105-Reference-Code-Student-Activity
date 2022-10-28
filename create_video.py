1026712a07ce2e53ed708b397660842b4c5ab192
nixmin23 <86486020+nixmin23@users.noreply.github.com>
2022-10-28 19:38:41 -0400

    Create create_video.py

---
+++ create_video.py
@@ -0,0 +1,19 @@
+import os
+import cv2
+path = "Images"
+images = []
+for file in os.listdir(path):
+    name, ext = os.path.splitext(file)
+    if ext in ['.gif', '.jpg', '.png', '.jpeg', '.jfif']:
+        file_name = path+'/'+file
+        images.append(file_name)
+count = len(images)
+frame = cv2.imread(images[0])
+height, width, channels = frame.shape
+size = (width, height)
+out = cv2.VideoWriter('project.mp4',cv2.VideoWriter_fourcc(*'DIVX'), 5, size, channels)
+for i in range(count-1,0,1):
+    frame = cv2.imread(images[i])
+    out.write(frame)
+out.release()
+print("Done")
